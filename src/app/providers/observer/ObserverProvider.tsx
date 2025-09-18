fa4ad818d45b8cd0b3e94ead902688e4b17c51c0
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-09-18 08:46:40 +0100

    observer config

--- src/app/providers/observer/ObserverProvider.tsx
+++ src/app/providers/observer/ObserverProvider.tsx
@@ -14,11 +14,24 @@ const ObserverContext = createContext<Payload | undefined>(undefined)
 const ObserverProvider = ({children} : any) => {
 
    const storageRef = useRef<Map<Element, (entry: IntersectionObserverEntry) => void>>(new Map());
+   // Strorage ref allows us to remember which element is linked to which callback function 
+
    const intersectionObserverRef = useRef<IntersectionObserver | null >(null)
+   // intersectionObserverRef will allows us to hold the actual intersectObserver
    
    
    const observerValue:Payload = {
       register : (element, callback)=>{
+         if(intersectionObserverRef.current == null){
+            let observer = new IntersectionObserver((entries)=>{
+               for (let entry of entries){
+                  if(storageRef.current.get(entry.target)){
+
+                  }
+               }
+            }, {})
+            intersectionObserverRef.current = observer
+         }
          return
       }, 
       unregister: (element)=>{
