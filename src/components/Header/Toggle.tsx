f0126c2b78ced69320fdd5a9df1ee9ffd31e6417
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-10-03 15:02:21 +0100

    Changed opcaity of theme

--- src/components/Header/Toggle.tsx
+++ src/components/Header/Toggle.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
+import { PiMoonLight, PiSunLight } from 'react-icons/pi';
 
 const Toggle = () => {
    const {theme, setTheme} = useTheme()
@@ -9,7 +10,7 @@ const Toggle = () => {
       <div>
          <button onClick = {()=> setTheme(prev => prev === "light" ? "dark": "light")}className = " ml-auto hover:cursor-pointer">
                {
-                  theme === "light" ? <Moon /> : <Sun />
+                  theme === "light" ? <PiMoonLight className="text-2xl hover:opacity-70"/> : <PiSunLight className="text-2xl hover:opacity-80" />
                } 
          </button>
          
