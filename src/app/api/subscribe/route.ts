e1224c8846a08d49ec264d3437c212c83696f81f
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-10-17 20:13:17 +0100

    Created post request in route.ts

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -1,4 +1,23 @@
-export async function POST(req: Request){
+export const POST = async(request: Request)=> {
+   const body = await request.json();
+   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+   const validateEmail = (email:string):boolean => {
+      return emailRegex.test(email)
+   }
 
 
+
+   if (body.email && validateEmail(body.email)){
+
+      return new Response(
+         JSON.stringify({message : "Email added", body}), 
+         { status: 201 }
+      )
+   }
+   else {
+      return new Response(JSON.stringify({message: "Error: No email "}), {
+         status: 400
+      })
+   }
 }   // return new Response("hello world")
