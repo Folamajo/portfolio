cecd4383b11e95bb0eff50cfedafe710d4319c89
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-10-20 16:55:52 +0100

    Added subscription payload

--- src/app/api/subscribe/route.ts
+++ src/app/api/subscribe/route.ts
@@ -2,7 +2,7 @@ export const runtime = "nodejs";
 
 const mailerLiteKey = process.env.MAILERLITE_API_KEY
 const mailerLiteGroupId = process.env.MAILERLITE_GROUP_ID
-const next_public_site_url = process.env.NEXT_PUBLIC_SITE_URL
+const nextPublicSiteUrl = process.env.NEXT_PUBLIC_SITE_URL
 
 
 
@@ -10,17 +10,39 @@ export const POST = async(request: Request)=> {
    // Post route accepts data and validates the email data 
    // Returns value to the user
    // Resoponse returns an object and accepts two parameters a body which is usually a message we want to send to backend  and options
+
+
+   
+
+
+
+   //Guard against missing config 
+   if (!mailerLiteKey || !mailerLiteGroupId){
+      return new Response(
+         JSON.stringify({message: "Server misconfiguration: missing env vars"}),
+         {status: 500}
+      )
+
+   }
    const body = await request.json();
+
    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
    const validateEmail = (email:string):boolean => {
       return emailRegex.test(email)
-   }
+   };
 
 
 
    if (body.email && validateEmail(body.email)){
 
+
+      const subscriptionPayload = {
+         email : body.email,
+         groupId : mailerLiteGroupId
+      }
+
+      
       return new Response(
          JSON.stringify({message : "Email added", body}), 
          { status: 201 }
